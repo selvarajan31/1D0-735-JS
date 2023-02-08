4beda893d0774b39b0cf660bc60b422234986ddc
nselvarajan <nselvarajan@outlook.com>
2023-02-08 22:38:05 +0530

    str.rplace and use of special symbol combination - started

--- scripts/regex.js
+++ scripts/regex.js
@@ -1,4 +1,28 @@
-let str = "We will, we will rock you";
-const result = str.match(/we/ig);   // i-case insensitive; g-global search
+const str = "We will, we will rock you";
+
+//str.match(regex)
+let result = str.match(/we/ig);   // i-case insensitive; g-global search
 alert(result);
-alert(result.index)
+
+result = str.match(/we/i);
+alert(`index: ${result.index}`) //only works without g flag
+alert(result.input)
+
+//str.replace(regex, replacement)
+const regexGI = /we/gi;
+const regexI = /we/i;
+
+result = str.replace(regexGI,"I");
+alert(`regexGI: ${result}`)
+
+result = str.replace(regexI,"I");
+alert(`regexI: ${result}`)
+
+//Special Synmbol Combinations
+// $& - matched string + replacement is inserted
+result = str.replace(regexGI,"$& all");
+alert(`regexI and $&: ${result}`);
+
+//$` inserts replacement text before matched string
+result = str.replace(regexGI,"$`"); //need to know how it works
+alert("regexI and $`: "+ result);
